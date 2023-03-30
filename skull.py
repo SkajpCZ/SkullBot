6ce5a6587f3903e0fea1af720bb94da8bcaebd20
Skajpik <lukyminecra@gmail.com>
2023-03-30 18:26:04 +0200

    Update skull.py

--- skull.py
+++ skull.py
@@ -16,7 +16,7 @@ async def on_ready(): clear(); await bot.change_presence(status=discord.Status.o
 async def ip(interaction: discord.Interaction):
     skulllist = ['https://media.tenor.com/aTljWQ18YeUAAAAd/rotating-skull.gif', 'https://media.tenor.com/epwlmzlBnAUAAAAC/skull.gif', 'https://media.tenor.com/ieXO0Ui-EoMAAAAC/skull-explode.gif', 'https://media.tenor.com/XmEgf6XjPRQAAAAd/skull.gif', 'https://media.tenor.com/g1bZgt4-tL4AAAAC/skull.gif', 'https://media.tenor.com/7_ZjV-cIL9YAAAAd/skull-grin.gif']
     selgif = random.randrange(len(skulllist))
-    embed = discord.Embed(title="💀", timestamp=datetime.utcnow()).set_image(url=skulllist[selgif]).set_footer(text="By DeadSkajp#5906")
+    embed = discord.Embed(title="💀", timestamp=datetime.utcnow()).set_image(url=skulllist[selgif]).set_footer(text="By Skajpik#3232")
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
 bot.run(token)
